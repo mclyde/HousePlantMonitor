a6d716dbf8ce999b71cbd85352546147c67001d2
mclyde <mclyde122@gmail.com>
2014-07-20 14:33:03 -0700

    Web page links added

--- app/views.py
+++ app/views.py
@@ -1,11 +1,28 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, url_for, redirect, Response
+import arduinoreaddemo
 
 @app.route('/')
 @app.route('/index')
 def index():
-	devices = [
-			{'name':'light meter', 'pins':['1', '2', '3'], 'active':'true'},
-			{'name':'soil moisture sensor', 'pins':['4', '5'], 'active':'true'}
+	return render_template("index.html", title = 'Index')
+
+@app.route('/config')
+def config():
+	presets = [
+			{'name':'temp', 'pin':'A0'},
+			{'name':'light', 'pin':'A1'},
+			{'name':'button', 'pin':'13'}
 	]
-	return render_template("index.html", title = 'Home', devices = devices)
+	return render_template("config.html", title = 'Sensor Configuration', presets = presets)
+
+@app.route('/email')
+def email():
+
+	return render_template("email.html", title = 'Communication Settings')
+
+@app.route('/readings')
+def readings():
+
+	return render_template("readings.html", title = 'Current Readings')
+
