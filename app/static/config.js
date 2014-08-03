ae85ecb5a0e993440fc9bfc30978f7d607f971aa
mclyde <mclyde122@gmail.com>
2014-08-02 19:02:40 -0700

    Finally got the @*#$&% dropdowns working

--- app/static/config.js
+++ app/static/config.js
@@ -3,26 +3,21 @@
  * Licensed under GPL v2 (https://github.com/mclyde/HousePlantMonitor/blob/master/LICENSE)
  * ===================================================================================== */
 
-$(document).ready(function() { $('body').hide().fadeIn(1000); });
+var inputs = [
+	{display:'Photometer', value:'light'},
+	{display:'Soil Monitor', value:'soil'},
+	{display:'Thermometer', value:'temp'}
+];
+var outputs = [
+	{display:'Email', value:'email'},
+	{display:'Text Message', value:'text'},
+	{display:'Tweet', value:'tweet'},
+	{display:'Motor: Shades', value:'shade'},
+	{display:'Motor: Water', value:'water'}
+];
 
-var deviceTypes = {
-	'inputs':  ['Photometer', 'Soil Monitor', 'Thermometer'],
-	'outputs': ['Email', 'Text Message', 'Tweet',  'Motor: Shades', 'Motor: Water']
-}
-
-var $types = $('select.deviceClass');
-var $subset = $('select.subset');
-$types.change(function () {
-	$subset.empty().append(function() {
-		var displayList = '';
-		$.each(deviceTypes[$types.val()], function(item) {
-			displayList += '<option>' + item + '</option>';
-		});
-		return displayList;
-	});
-}).change();
-
-$(function () { 
+$(function () {
+	$('body').hide().fadeIn(1000);
     $("[data-toggle='popover']").popover({	container:'body',
     										html:'true',
     										placement:'bottom',
@@ -33,6 +28,31 @@ $(function () {
 											content: function() {
 												return $('#popover-content').html();
 											}
-    									}); 
+    									});
 });
+
+$(document).on('change', 'select.deviceClass', function() {
+	var key = $(this).val();
+	console.log(key);
+	switch(key) {
+		case 'inputs':
+			list(inputs);
+			break;
+		case 'outputs':
+			list(outputs);
+			break;
+		default:
+			break;
+	}
+});
+
+function list(arr) {
+	$('.subset').html("");
+	$(arr).each(function(i) {
+		$('.subset').append("<option value=\"" + arr[i].value + "\">" + arr[i].display + "</option>");
+	});
+}
+
+
+
 
--- app/views.py
+++ app/views.py
@@ -76,7 +76,7 @@ def config():
 		troopPins[troop.name] = scoutPins
 
 	return render_template("config.html", title = 'Sensor Configuration', troopPins = troopPins
-		, troops = account.troops, inputs = DeviceTypes.inputs, outputs = DeviceTypes.outputs)
+		, troops = account.troops)
 
 @app.route('/communications', methods=['GET', 'POST'])
 def communications():
